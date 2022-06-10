0cf50199e509ea59a50797afab8764d2c57ac332
arisac <hi@aris.ac>
2022-06-10 20:53:57 +0800

    fix: theme swich breaks web3 provider hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,18 +9,42 @@ import { Chain, chain, createClient, configureChains, WagmiConfig } from 'wagmi'
 import { infuraProvider } from 'wagmi/providers/infura'
 import { publicProvider } from 'wagmi/providers/public'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { useTheme } from 'next-themes'
 import { app } from 'appConfig'
-import { useState, useEffect } from 'react'
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, gnosisChain],
+    [
+      infuraProvider({ infuraId: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
+      jsonRpcProvider({
+        rpc: chain => {
+          if (chain.id !== gnosisChain.id) return null
+          return { http: chain.rpcUrls.default }
+        },
+      }),
+      publicProvider(),
+    ]
+  )
+  const { connectors } = getDefaultWallets({ appName: app.name, chains })
+  const wagmiClient = createClient({ autoConnect: true, connectors, provider })
+
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <HeadGlobal />
-      <Web3Wrapper>
-        <Component key={router.asPath} {...pageProps} />
-      </Web3Wrapper>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          showRecentTransactions={true}
+          // Currently theme color is only following system, not the app theme
+          theme={{
+            lightMode: lightTheme(),
+            darkMode: darkTheme(),
+          }}
+        >
+          <Component key={router.asPath} {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ThemeProvider>
   )
 }
@@ -43,41 +67,4 @@ const gnosisChain: Chain = {
     default: { name: 'BlockScout', url: 'https://blockscout.com/xdai/mainnet' },
   },
   testnet: false,
-}
-
-// Web3Wrapper
-export function Web3Wrapper({ children }) {
-  const { chains, provider } = configureChains(
-    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, gnosisChain],
-    [
-      infuraProvider({ infuraId: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
-      jsonRpcProvider({
-        rpc: chain => {
-          if (chain.id !== gnosisChain.id) return null
-          return { http: chain.rpcUrls.default }
-        },
-      }),
-      publicProvider(),
-    ]
-  )
-  const { connectors } = getDefaultWallets({ appName: app.name, chains })
-  const wagmiClient = createClient({ autoConnect: true, connectors, provider })
-
-  const [mounted, setMounted] = useState(false)
-  const { resolvedTheme } = useTheme()
-
-  useEffect(() => setMounted(true), [])
-  if (!mounted) return null
-
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        showRecentTransactions={true}
-        theme={resolvedTheme === 'dark' ? darkTheme() : lightTheme()}
-      >
-        {children}
-      </RainbowKitProvider>
-    </WagmiConfig>
-  )
 }
