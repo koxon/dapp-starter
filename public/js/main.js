aa331cac62707a737fbbf6d9b1b08c10b8d890b7
Nicolas Menciere <nmenciere@bfansports.com>
2023-08-27 19:06:50 -0400

    Add styles

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+WebFont.load({
+  google: {
+    families: [
+      'Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic',
+      'Inter:300,regular,500,600,800:cyrillic,latin',
+    ],
+  },
+})
+
+!(function (o, c) {
+  var n = c.documentElement,
+    t = ' w-mod-'
+  ;(n.className += t + 'js'),
+    ('ontouchstart' in o || (o.DocumentTouch && c instanceof DocumentTouch)) && (n.className += t + 'touch')
+})(window, document)
